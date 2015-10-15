b41c967ac171141ac5c1c459f92560d80930d1be
Greg V <greg@unrelenting.technology>
2015-10-15 14:16:51 +0300

    Add FreeBSD support

--- src/lib.rs
+++ src/lib.rs
@@ -89,6 +89,11 @@
 //!     cfsetspeed(termios, termios::os::macos::B230400)
 //! }
 //!
+//! #[cfg(target_os = "freebsd")]
+//! fn set_fastest_speed(termios: &mut Termios) -> io::Result<()> {
+//!     cfsetspeed(termios, termios::os::freebsd::B230400)
+//! }
+//!
 //! # let fd = 1;
 //! let mut termios = Termios::from_fd(fd).unwrap();
 //! set_fastest_speed(&mut termios).unwrap();
@@ -106,7 +111,8 @@ use libc::{c_int,pid_t};
 pub use ::os::target::{cc_t,speed_t,tcflag_t}; // types
 pub use ::os::target::{VEOF,VEOL,VERASE,VINTR,VKILL,VMIN,VQUIT,VSTART,VSTOP,VSUSP,VTIME}; // c_cc subscripts
 pub use ::os::target::{BRKINT,ICRNL,IGNBRK,IGNCR,IGNPAR,INLCR,INPCK,ISTRIP,IXANY,IXOFF,IXON,PARMRK}; // input modes
-pub use ::os::target::{OPOST,ONLCR,OCRNL,ONOCR,ONLRET,OFILL,NLDLY,NL0,NL1,CRDLY,CR0,CR1,CR2,CR3,TABDLY,TAB0,TAB1,TAB2,TAB3,BSDLY,BS0,BS1,VTDLY,VT0,VT1,FFDLY,FF0,FF1}; // output modes
+#[cfg(any(target_os = "linux", target_os = "macos"))] pub use ::os::target::{OPOST,ONLCR,OCRNL,ONOCR,ONLRET,OFILL,NLDLY,NL0,NL1,CRDLY,CR0,CR1,CR2,CR3,TABDLY,TAB0,TAB1,TAB2,TAB3,BSDLY,BS0,BS1,VTDLY,VT0,VT1,FFDLY,FF0,FF1}; // output modes
+#[cfg(target_os = "freebsd")] pub use ::os::target::{OPOST,ONLCR,TABDLY,TAB0,TAB3,ONOEOT,OCRNL,ONOCR,ONLRET}; // output modes
 pub use ::os::target::{B0,B50,B75,B110,B134,B150,B200,B300,B600,B1200,B1800,B2400,B4800,B9600,B19200,B38400}; // baud rate selection
 pub use ::os::target::{CSIZE,CS5,CS6,CS7,CS8,CSTOPB,CREAD,PARENB,PARODD,HUPCL,CLOCAL}; // control modes
 pub use ::os::target::{ECHO,ECHOE,ECHOK,ECHONL,ICANON,IEXTEN,ISIG,NOFLSH,TOSTOP}; // local modes
--- src/os/mod.rs
+++ src/os/mod.rs
@@ -2,6 +2,8 @@
 
 #[cfg(target_os = "linux")] pub use self::linux as target;
 #[cfg(target_os = "macos")] pub use self::macos as target;
+#[cfg(target_os = "freebsd")] pub use self::freebsd as target;
 
 #[cfg(target_os = "linux")] pub mod linux;
 #[cfg(target_os = "macos")] pub mod macos;
+#[cfg(target_os = "freebsd")] pub mod freebsd;
