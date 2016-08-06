abb2e373b8c75e693dff9b507dbd4530efe67290
Michael Neumann <mneumann@ntecs.de>
2016-08-06 12:58:33 +0200

    Add support for DragonFlyBSD
    Reuse the freebsd module, but export it as "dragonfly".

--- src/os/mod.rs
+++ src/os/mod.rs
@@ -3,9 +3,11 @@
 #[cfg(target_os = "linux")] pub use self::linux as target;
 #[cfg(target_os = "macos")] pub use self::macos as target;
 #[cfg(target_os = "freebsd")] pub use self::freebsd as target;
+#[cfg(target_os = "dragonfly")] pub use self::dragonfly as target;
 #[cfg(target_os = "openbsd")] pub use self::openbsd as target;
 
 #[cfg(target_os = "linux")] pub mod linux;
 #[cfg(target_os = "macos")] pub mod macos;
 #[cfg(target_os = "freebsd")] pub mod freebsd;
+#[cfg(target_os = "dragonfly")] #[path = "freebsd.rs"] pub mod dragonfly;
 #[cfg(target_os = "openbsd")] pub mod openbsd;
