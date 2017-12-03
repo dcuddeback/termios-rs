eaa538631796b71cd341f78f193ea742f32fe385
David Cuddeback <david.cuddeback@gmail.com>
2017-12-03 15:01:49 -0700

    update documentation for v0.3

--- implementors/core/clone/trait.Clone.js
+++ implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['termios'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='termios/os/linux/struct.termios.html' title='termios::os::linux::termios'>termios</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='termios/struct.Termios.html' title='termios::Termios'>Termios</a>",];
+implementors["termios"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"termios/os/linux/struct.termios.html\" title=\"struct termios::os::linux::termios\">termios</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"termios/struct.Termios.html\" title=\"struct termios::Termios\">Termios</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
---
+++ implementors/core/ops/deref/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["termios"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"termios/struct.Termios.html\" title=\"struct termios::Termios\">Termios</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
